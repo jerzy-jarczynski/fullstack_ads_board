d37b88ebe2d92097a03b22e086e45a1ff55ebb22
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-08-13 16:03:01 +0200

    Modify ad model

--- models/Ad.model.js
+++ models/Ad.model.js
@@ -1,7 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const adSchema = new mongoose.Schema({
-
+  title: { type: String, required: true, minLength: 10, maxLength: 50 },
+  description: { type: String, required: true, minLength: 20, maxLength: 1000 },
+  publishDate: { type: Date, required: true },
+  image: { type: String, required: true },
+  price: { type: Number, required: true },
+  location: { type: String, required: true },
+  userId: { type: String, required: true, ref: "User" },
 });
 
-module.exports = mongoose.model('Ad', adSchema);
+module.exports = mongoose.model("Ad", adSchema);
