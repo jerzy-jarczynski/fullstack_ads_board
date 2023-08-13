3b73555d307d50f258064b6d9abf4b8fac9a271e
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-08-13 16:00:49 +0200

    Modify database connection configuration

--- db.js
+++ db.js
@@ -1,18 +1,29 @@
 const mongoose = require('mongoose');
 
+const uri = `mongodb+srv://anderfor:${process.env.DB_PASS}@kodilla.vpc6m3e.mongodb.net/NewWaveDB?retryWrites=true&w=majority`;
+
+const NODE_ENV = process.env.NODE_ENV;
+let dbUri = '';
+
+if (NODE_ENV === 'production') dbUri = uri;
+else if (NODE_ENV === 'test') dbUri = 'mongodb://localhost:27017/adsDBTest';
+else dbUri = 'mongodb://localhost:27017/NewWaveDB';
+
 const connectToDB = () => {
 
-  // connect to DB
-  mongoose.connect('mongodb://localhost:27017/adsDB', { useNewUrlParser: true, useUnifiedTopology: true });
-  const db = mongoose.connection;
-
-  // on success
-  db.once('open', () => {
-    console.log('Connected to the database');
-  });
-
-  // on error
-  db.on('error', (err) => console.log('Error ' + err));
+  try {
+    mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
+    const db = mongoose.connection;
+  
+    db.once('open', () => {
+      if (NODE_ENV !== "test") console.log("Connected to the database");
+    });
+    db.on('error', err => console.log('Error ' + err));
+  } catch (err) {
+    if(process.env.debug === true) console.log(err);
+    else console.log('Couldn\'t connect to db...');
+  }
+  
 }
 
 module.exports = connectToDB;
