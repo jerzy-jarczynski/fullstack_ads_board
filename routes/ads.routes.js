38afa4d26b245677e9bd5588505a10eabb662692
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-08-13 16:13:21 +0200

    Add routes for ads

--- routes/ads.routes.js
+++ routes/ads.routes.js
@@ -3,4 +3,11 @@ const router = express.Router();
 
 const ads = require('../controllers/ads.controller');
 
+router.get('/ads', ads.getAll);
+router.get('/ads/:id', ads.getById);
+router.get('/ads/search/:searchPhrase', ads.getBySearchPhrase);
+router.post('/ads', ads.addNew);
+router.detele('/ads/:id', ads.removeById);
+router.put('/ads/:id', ads.modifyById);
+
 module.exports = router;
