cf9a3eed1b9064999dc86f2d8f1c84ff92f21a0e
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-08-15 21:34:20 +0200

    Add image uploading with multer

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,18 +1,21 @@
 const User = require('../models/User.model');
 const bcrypt = require('bcryptjs');
+const getImageFileType = require('../utils/getImageFileType');
 
 exports.register = async (req, res) => {
   try {
 
     const { login, password } = req.body;
+    const fileType = req.file ? await getImageFileType(req.file) : 'unknown';
 
-    if (login && typeof login === 'string' && password && typeof password === 'string') {
+    if (login && typeof login === 'string' && password && typeof password === 'string' && req.file && ['image/png', 'image/jpeg', 'image/gif'].includes(fileType)) {
+
       const userWithLogin = await User.findOne({ login });
       if (userWithLogin) {
         return res.status(409).send({ message: 'User with this login already exists' });
       }
 
-      const user = await User.create({ login, password: await bcrypt.hash(password, 10) });
+      const user = await User.create({ login, password: await bcrypt.hash(password, 10), avatar: req.file.filename });
       res.status(201).send({ message: 'User created ' + user.login });
     } else {
       res.status(400).send({ message: 'Bad request' });
