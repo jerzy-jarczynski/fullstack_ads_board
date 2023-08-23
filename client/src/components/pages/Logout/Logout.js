075257666b58460abc8b0818c2b40bdd686226fd
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-08-23 22:41:55 +0200

    Add logout to the client

--- client/src/components/pages/Logout/Logout.js
+++ client/src/components/pages/Logout/Logout.js
@@ -1,10 +1,27 @@
+import { useDispatch } from "react-redux";
+import { API_AUTH_URL } from "../../../config";
+import { logOut } from "../../../redux/usersRedux";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Logout = () => {
-  
-  return (
-    <div>
-      Logout
-    </div>
-  );
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const options = {
+      method: "DELETE",
+      credentials: "include",
+    };
+
+    fetch(`${API_AUTH_URL}/logout`, options).then(() => {
+      dispatch(logOut());
+      navigate("/");
+    });
+  }, [dispatch, navigate]);
+
+  return null;
 
 };
 
